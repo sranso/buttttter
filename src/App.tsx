6e84e2523bd8f21465bf39dedc3e8b6b895ef16a
Sarah Ransohoff <sarah.ransohoff@clay.run>
2026-07-16 10:53:06 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-yellow-50">
+      <h1 className="text-6xl font-bold text-yellow-900">butter</h1>
+    </main>
+  )
+}
